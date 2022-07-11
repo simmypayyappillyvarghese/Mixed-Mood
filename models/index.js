a7a13dabcbb3327a105e1fdd619feeb512359819
simmy varghese <simmyvarghese5@gmail.com>
2022-07-10 18:12:14 -0700

    Added the table relationship

--- models/index.js
+++ models/index.js
@@ -1,6 +1,30 @@
 
 const User=require('./User');
+const Song=require('./Song');
+const Library=require('./Library');
+
 
 //Add the relation between the tables
+
+
+User.belongsToMany(Song,{
+
+    through:{
+  
+        model:Library,
+    },
+    as:'user_song_list'
+  });
+  
+  // Tags belongToMany Products (through ProductTag)
+  
+  Song.belongsToMany(User,{
+  
+    through:{
+  
+        model:Library,
+    },
+    as:'song_of_users'
+  });
 
 module.exports={User};
