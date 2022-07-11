153025445ae5e6463912a5a7823a193b2ce83b73
jesus villegas <jesusvicajr@hotmail.com>
2022-07-11 14:06:58 -0700

    changes

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,7 +3,7 @@ const { User,Song,Library } = require('../models');
 
 const userData = require('./userData.json');
 //Uncomment once data is added
-// const songData =require('./songData.json');
+ const songData =require('./songData.json');
 // const libraryData =require('./libraryData.json');
 
 const seedDatabase = async () => {
@@ -15,7 +15,10 @@ const seedDatabase = async () => {
   });
 
   //Bulk Create for song data
-
+  await Song.bulkCreate(songData, {
+    individualHooks: true,
+    returning: true,
+  });
 
 
  //Bulk Create for library data
