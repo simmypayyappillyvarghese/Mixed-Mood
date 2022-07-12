b5eacd323883fb26da2a418c6b543de3ab8d7e7c
Daniel <dherzog100@gmail.com>
2022-07-12 15:20:12 -0700

    added files

---
+++ public/js/add-song-library.js
@@ -0,0 +1,33 @@
+
+const myLibrary=[];
+
+const library=document.querySelector('.song-list-container');
+
+async function addSongToLibrary(event){
+
+     //Verifies the User clicked the Play Icon
+      if(event.target.classList.contains('fa-heart')){
+       
+        const songId=event.target.parentElement.parentElement.dataset.id;
+    
+        const response=await fetch('/api/user/saveSong',
+        {
+            method:'POST',
+            body:JSON.stringify({songId}),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        console.log(response);
+
+        if(response.ok){
+
+            console.log("To do -Display the Success Message");
+            window.location.replace('/home/save');
+        }
+        }
+        else{
+            return;
+        }
+}
+
+playList.addEventListener('click',addSongToLibrary);
