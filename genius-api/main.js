beb80148462f4fc88cc2908a105e18192f82ee42
onionchop <ruthhyo@gmail.com>
2022-07-13 13:54:20 -0400

    tested

--- genius-api/main.js
+++ genius-api/main.js
@@ -2,8 +2,8 @@ const getLyrics=require("./getLyrics")
 const getSong=require("./getSong")
 const options={
     apiKey:'Co1fAveFWufOGFGWDduNUWWtfkeFmWAMeids04ONZe_fuRTkyjfkjZISim065H5-',
-    title:'for whom the bell tolls',
-    artist:'metallica',
+    title:'grinding halt',
+    artist:'the cure',
     optimizeQuery:true
 }
 getLyrics(options).then((lyrics)=>console.log(lyrics));
